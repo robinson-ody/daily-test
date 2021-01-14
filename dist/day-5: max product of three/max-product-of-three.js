170e210995ce702b83d2dfbb819c4ff238be75a0
Robinson Ody <robinsonody@Robinson-Ody.local>
2021-01-14 17:38:51 +0700

    Max Product of Three

--- dist/day-5: max product of three/max-product-of-three.js
+++ dist/day-5: max product of three/max-product-of-three.js
@@ -1,22 +1,51 @@
 "use strict";
 console.clear();
-const input = [-4, -4, 2, 8];
-const input2 = [-4, 2, 8, -4];
-const input3 = [2, -4, -4, 8];
 const maxProductOfThree = (numbers) => {
-    console.log({ numbers });
-    let simpan = null;
+    const startTime = new Date().getTime();
+    console.log(`Start at ${startTime}`);
+    let willCalculate = [];
+    const positive = [];
+    let negative = [];
     for (let i in numbers) {
         if (numbers[i] !== 0) {
-            if (numbers[i] < 0 && simpan === null)
-                simpan = numbers[i];
-            else if (numbers[i] < 0 && simpan !== null)
-                console.log(`Ngapain?`);
-            else
-                console.log(`Ngapain 2?`);
+            const current = numbers[i];
+            if (current < 0)
+                negative.push(current);
+            else if (current > 0)
+                positive.push(current);
         }
     }
-    return 0;
+    if (negative.length % 2)
+        negative.shift();
+    for (let i in negative)
+        negative[i] = Math.abs(negative[i]);
+    willCalculate = [...negative, ...positive];
+    willCalculate.sort((a, b) => a - b);
+    const finalThree = willCalculate.slice(willCalculate.length - 3);
+    let returnString = '';
+    finalThree.forEach((num, index) => {
+        returnString += num;
+        if (index < 2)
+            returnString += ' * ';
+        if (index === 2)
+            returnString += ' = ';
+    });
+    returnString += finalThree.reduce((a, b) => a * b);
+    const endTime = new Date().getTime();
+    console.log(`End at ${endTime}`);
+    console.log(`Duration: ${(endTime - startTime) / 1000}s`);
+    return returnString;
 };
-console.log(maxProductOfThree([-4, -4, 2, 8]));
+const min = -1000;
+const max = 1000;
+const randArrLen = 10;
+let i = 0;
+const randArr = [];
+const getRndInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+while (i < randArrLen) {
+    randArr.push(getRndInteger(min, max));
+    i++;
+}
+console.log({ randArr });
+console.log(maxProductOfThree(randArr));
 //# sourceMappingURL=max-product-of-three.js.map
